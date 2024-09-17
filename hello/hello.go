c3d0ad872faddb9396b2486d2888b45c3a4279a5
Jay <jaytpa01@gmail.com>
2024-09-17 22:32:51 +1000

    refactor(hello): create private function for handling the language prefix

--- hello/hello.go
+++ hello/hello.go
@@ -9,9 +9,14 @@ func main() {
 }
 
 const (
-	spanish            = "Spanish"
+	spanish = "Spanish"
+	french  = "French"
+	german  = "German"
+
 	englishHelloPrefix = "Hello, "
 	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	germanHelloPrefix  = "Hallo, "
 )
 
 func Hello(name, language string) string {
@@ -19,8 +24,19 @@ func Hello(name, language string) string {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case german:
+		prefix = germanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name
+	return
 }
